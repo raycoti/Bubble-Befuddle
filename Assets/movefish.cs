521b40c00a2b8fdab264f52070026a2e73088857
Raymond Coti <raymond.coti@gmail.com>
2016-09-10 16:24:15 -0500

    Add files via upload

---
+++ Assets/movefish.cs
@@ -0,0 +1,49 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using TouchScript.Gestures;
+using System;
+using Random = UnityEngine.Random;
+public class movefish : MonoBehaviour {
+	private Vector2 startPos = new Vector2();
+	public float speed=1.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	private void OnEnable()
+	{
+		// press/release gesture for adding force like slingshot
+		GetComponent<PressGesture>().Pressed += pressedHandler;
+		GetComponent<ReleaseGesture>().Released += releasedHandler;
+		
+		//
+	}
+	
+	private void OnDisable()
+	{
+		// remove all registered callback function
+		GetComponent<PressGesture>().Pressed -= pressedHandler;
+		GetComponent<ReleaseGesture>().Released -= releasedHandler;
+
+	}
+	
+	private void pressedHandler(object sender, EventArgs e)
+	{
+		// record press point
+		var gesture = sender as PressGesture;
+		startPos = gesture.NormalizedScreenPosition;
+	}
+	private void releasedHandler(object sender, EventArgs e)
+	{
+		// add force!
+		var gesture = sender as ReleaseGesture;
+		Vector2 force = gesture.NormalizedScreenPosition - startPos;
+		GetComponent<Rigidbody2D>().AddForce (force*speed);
+	}
+}
---
+++ Assets/spongetime.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spongetime : MonoBehaviour {
+	float timer;
+	// Use this for initialization
+	void Start () {
+		timer = Time.time + 20.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timer < Time.time) {
+			Destroy (this.gameObject);
+		}
+	}
+}
